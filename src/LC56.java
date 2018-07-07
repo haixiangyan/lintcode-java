80299c3571b259b6450f11becba2025f57f7cf50
Haixiang <haixiang6123@gmail.com>
2018-07-07 20:52:39 +0800

    Solve 56

--- src/LC56.java
+++ src/LC56.java
@@ -1,33 +1,26 @@
+import java.util.HashMap;
+
 public class LC56 {
     public int[] twoSum(int[] numbers, int target) {
-        int[] pairs = {-1, -1};
+        HashMap<Integer, Integer> map = new HashMap<>();
 
-        int left = 0;
-        int right = numbers.length - 1;
-
-        while (left < right) {
-            if (numbers[left] + numbers[right] == target) {
-                pairs[0] = left;
-                pairs[1] = right;
-                return pairs;
+        for (int i = 0; i < numbers.length; i++) {
+            if (map.get(numbers[i]) != null) {
+                int[] result = { map.get(numbers[i]), i };
+                return result;
             }
-
-            if (numbers[left] + numbers[right] < target) {
-                left ++;
-            }
-            else {
-                right --;
-            }
+            map.put(target - numbers[i], i);
         }
 
-        return null;
+        int[] result = {};
+        return result;
     }
 
     public static void main(String[] args) {
         LC56 lc56 = new LC56();
 
-        int[] numbers = {1};
-        int target = 1;
+        int[] numbers = {1, 0, -1};
+        int target = -1;
 
         int[] paris = lc56.twoSum(numbers, target);
 
