f0f335554c417c4352503e410987234f2c73ff7d
Haixiang <haixiang6123@gmail.com>
2018-07-08 21:42:43 +0800

    Solve 103

---
+++ src/LC103.java
@@ -0,0 +1,45 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+public class LC103 {
+    public ListNode detectCycle(ListNode head) {
+        ListNode intersection = chase(head);
+        // No cycle
+        if (intersection == null) {
+            return null;
+        }
+
+        ListNode line = head;
+        ListNode cycle = intersection;
+
+        while (line != cycle) {
+            line = line.next;
+            cycle = cycle.next;
+        }
+
+        return line;
+    }
+
+    private ListNode chase(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                return fast;
+            }
+        }
+
+        return null;
+    }
+}
