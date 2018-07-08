79aed9239695cfbb1d6137de4289ae015f504935
Haixiang <haixiang6123@gmail.com>
2018-07-08 21:31:43 +0800

    Solve 102

---
+++ src/LC102.java
@@ -0,0 +1,28 @@
+public class LC102 {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+            this.next = null;
+        }
+    }
+
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            // If slow can catch up fast then means it has a cycle
+            if (fast == slow) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
