d7048b269526973ab2b69c71d7a75c4bf1de0a89
Haixiang <haixiang6123@gmail.com>
2018-07-09 13:15:06 +0800

    Solve 463 with merge sort

--- src/LC463.java
+++ src/LC463.java
@@ -1,15 +1,17 @@
 public class LC463 {
     public void sortIntegers(int[] A) {
         if (A == null || A.length == 0) {
-            return ;
+            return;
         }
 
-        quickSort(A, 0, A.length - 1);
+        int[] temp = new int[A.length];
+
+        mergeSort(A, 0, A.length - 1, temp);
     }
 
     private void quickSort(int[] A, int start, int end) {
         if (start >= end) {
-            return ;
+            return;
         }
 
         int left = start;
@@ -19,11 +21,11 @@ public class LC463 {
         while (left <= right) {
             // Find left element that is smaller than the pivot
             while (left <= right && A[left] < pivot) {
-                left ++;
+                left++;
             }
             // Find the right element that is larger than the pivot
             while (left <= right && A[right] > pivot) {
-                right --;
+                right--;
             }
 
             // Swap two element
@@ -31,13 +33,49 @@ public class LC463 {
                 int temp = A[left];
                 A[left] = A[right];
                 A[right] = temp;
-                left ++;
-                right --;
+                left++;
+                right--;
             }
         }
 
         quickSort(A, start, right);
         quickSort(A, left, end);
+    }
+
+    private void mergeSort(int[] A, int start, int end, int[] temp) {
+        if (start >= end) {
+            return;
+        }
+
+        mergeSort(A, start, (start + end) / 2, temp);
+        mergeSort(A, (start + end) / 2 + 1, end, temp);
+        merge(A, start, end, temp);
+    }
+
+    private void merge(int[] A, int start, int end, int[] temp) {
+        int middle = (start + end) / 2;
+        int leftIndex = start;
+        int rightIndex = middle + 1;
+        int index = leftIndex;
+
+        while (leftIndex <= middle && rightIndex <= end) {
+            if (A[leftIndex] < A[rightIndex]) {
+                temp[index++] = A[leftIndex++];
+            } else {
+                temp[index++] = A[rightIndex++];
+            }
+        }
+
+        while (leftIndex <= middle) {
+            temp[index++] = A[leftIndex++];
+        }
+        while (rightIndex <= end) {
+            temp[index++] = A[rightIndex++];
+        }
+
+        for (int i = start; i <= end; i++) {
+            A[i] = temp[i];
+        }
     }
 
     public static void main(String[] args) {
