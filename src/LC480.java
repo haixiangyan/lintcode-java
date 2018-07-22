73d57477c39814ac9a87e64da0a91fa2bf771e9b
Haixiang <haixiang6123@gmail.com>
2018-07-22 21:40:38 +0800

    Solve 480. Binary Tree Paths

---
+++ src/LC480.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class LC480 {
+    class TreeNode {
+        public int val;
+        public TreeNode left, right;
+
+        public TreeNode(int val) {
+            this.val = val;
+            this.left = this.right = null;
+        }
+    }
+
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> paths = new ArrayList<>();
+
+        if (root == null) {
+            return paths;
+        }
+
+        List<String> leftPaths = binaryTreePaths(root.left);
+        List<String> rightPaths = binaryTreePaths(root.right);
+
+        for (String path : leftPaths) {
+            paths.add(root.val + "->" + path);
+        }
+        for (String path : rightPaths) {
+            paths.add(root.val + "->" + path);
+        }
+
+        // If this is a leaf
+        if (paths.size() == 0) {
+            paths.add("" + root.val);
+        }
+
+        return paths;
+    }
+}
