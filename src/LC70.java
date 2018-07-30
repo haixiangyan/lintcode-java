aeb276e4faad6ffbbdb494cb3486b773e7501dcb
Haixiang <haixiang6123@gmail.com>
2018-07-30 16:15:54 +0800

    Solve 70. Binary Tree Level Order Traversal II

---
+++ src/LC70.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class LC70 {
+    class TreeNode {
+        public int val;
+        public TreeNode left, right;
+
+        public TreeNode(int val) {
+            this.val = val;
+            this.left = this.right = null;
+        }
+    }
+
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> results = new ArrayList<>();
+
+        if (root == null) {
+            return results;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> level = new ArrayList<>();
+
+            for (int i = 0; i < size; i++) {
+                TreeNode curNode = queue.poll();
+                level.add(curNode.val);
+
+                if (curNode.left != null) {
+                    queue.offer(curNode.left);
+                }
+                if (curNode.right != null) {
+                    queue.offer(curNode.right);
+                }
+            }
+
+            results.add(level);
+        }
+
+        Collections.reverse(results);
+        return results;
+    }
+}
