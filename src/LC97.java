b68b996cc3bbdd104815a60b424c6bc7b1e40460
Haixiang <haixiang6123@gmail.com>
2018-07-30 20:37:58 +0800

    Solve 97. Maximum Depth of Binary Tree

---
+++ src/LC97.java
@@ -0,0 +1,22 @@
+public class LC97 {
+    class TreeNode {
+        public int val;
+        public TreeNode left, right;
+
+        public TreeNode(int val) {
+            this.val = val;
+            this.left = this.right = null;
+        }
+    }
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+
+        return Math.max(left, right) + 1;
+    }
+}
