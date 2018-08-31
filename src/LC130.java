13351c3cd0d7aff889ea677aeccef42bdcf0a7b7
Haixiang <haixiang6123@gmail.com>
2018-08-31 20:47:09 +0800

    Solve 130. Heapify

---
+++ src/LC130.java
@@ -0,0 +1,22 @@
+public class LC130 {
+    private void siftup(int[] A, int k) {
+        while (k != 0) {
+            int father = (k - 1) / 2;
+            if (A[k] > A[father]) {
+                break;
+            }
+
+            int temp = A[k];
+            A[k] = A[father];
+            A[father] = temp;
+
+            k = father;
+        }
+    }
+
+    public void heapify(int[] A) {
+        for (int i = 0; i < A.length; i++) {
+            siftup(A, i);
+        }
+    }
+}
