a7cdb88e0c8fc80759c26c887ad3d4f811becfa4
Haixiang <haixiang6123@gmail.com>
2018-09-12 15:32:24 +0800

    Solve 30. Insert Interval

--- src/LC30.java
+++ src/LC30.java
@@ -1,42 +1,41 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class LC30 {
-    public int partitionArray(int[] nums, int k) {
-        if (nums == null || nums.length == 0) {
-            return 0;
+    class Interval {
+        int start, end;
+
+        Interval(int start, int end) {
+            this.start = start;
+            this.end = end;
         }
-
-        int left = 0;
-        int right = nums.length - 1;
-
-        while (left <= right) {
-            while (left <= right && nums[left] < k) {
-                left++;
-            }
-            while (left <= right && nums[right] >= k) {
-                right--;
-            }
-
-            if (left <= right) {
-                int temp = nums[left];
-                nums[left] = nums[right];
-                nums[right] = temp;
-                left++;
-                right--;
-            }
-        }
-
-        return right + 1;
     }
 
-    public static void main(String[] args) {
-        int[] nums = {3, 2, 2, 1};
-        int k = 2;
-
-        LC30 lc30 = new LC30();
-
-        System.out.println(lc30.partitionArray(nums, k));
-
-        for (int i : nums) {
-            System.out.println(i);
+    public List<Interval> insert(List<Interval> intervals, Interval newInterval) {
+        if (newInterval == null || intervals == null) {
+            return intervals;
         }
+
+        List<Interval> results = new ArrayList<>();
+        int insertPos = 0;
+
+        for (Interval interval : intervals) {
+            if (interval.end < newInterval.start) {
+                // Add current interval and new interval
+                results.add(interval);
+                insertPos++;
+            }
+            else if (interval.start > newInterval.end) {
+                results.add(interval);
+            }
+            else {
+                newInterval.start = Math.min(interval.start, newInterval.start);
+                newInterval.end = Math.max(interval.end, newInterval.end);
+            }
+        }
+
+        results.add(insertPos, newInterval);
+
+        return results;
     }
 }
---
+++ src/LC31.java
@@ -0,0 +1,42 @@
+public class LC31 {
+    public int partitionArray(int[] nums, int k) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            while (left <= right && nums[left] < k) {
+                left++;
+            }
+            while (left <= right && nums[right] >= k) {
+                right--;
+            }
+
+            if (left <= right) {
+                int temp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = temp;
+                left++;
+                right--;
+            }
+        }
+
+        return right + 1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {3, 2, 2, 1};
+        int k = 2;
+
+        LC31 lc30 = new LC31();
+
+        System.out.println(lc30.partitionArray(nums, k));
+
+        for (int i : nums) {
+            System.out.println(i);
+        }
+    }
+}
