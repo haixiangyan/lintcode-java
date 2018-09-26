d2e3c1dab4ed6783acb140512fad2200e125df48
Haixiang6123 <haixiang6123@gmail.com>
2018-09-26 14:08:13 -0700

    Solve 150. Best Time to Buy and Sell Stock II

---
+++ src/LC150.java
@@ -0,0 +1,13 @@
+public class LC150 {
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+
+        for (int i = 0; i < prices.length - 1; i++) {
+            int diff = prices[i + 1] - prices[i];
+            if (diff > 0) {
+                profit += diff;
+            }
+        }
+        return profit;
+    }
+}
