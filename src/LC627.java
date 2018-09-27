c86d0fe68c366d707861018b5c350d50429f5799
Haixiang6123 <haixiang6123@gmail.com>
2018-09-27 11:16:13 -0700

    Review 627. Longest Palindrome

--- src/LC627.java
+++ src/LC627.java
@@ -1,59 +1,23 @@
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
+import java.util.*;
 
 public class LC627 {
-
     public int longestPalindrome(String s) {
-        // Check validation
-        if (s == null || s.equals("")) {
-            return 0;
-        }
+        Set<Character> set = new HashSet<>();
 
-        HashMap<Character, Integer> charSet = new HashMap<>();
-        int total = 0;
-        boolean containOdd = false;
-
-        // Count number of each character
-        collectCharset(charSet, s);
-
-        // Count odd times and even times
-        for (Integer value : charSet.values()) {
-            if (value % 2 == 0) {
-                total = total + value;
+        for (char c : s.toCharArray()) {
+            if (set.contains(c)) {
+                set.remove(c);
             }
             else {
-                containOdd = true;
-                total = total + value - 1;
+                set.add(c);
             }
         }
 
-        total = containOdd ? total + 1 : total;
-
-        return total;
-    }
-
-    private void collectCharset(HashMap<Character, Integer> charSet, String s) {
-        int number;
-        char curChar;
-        int strLen = s.length();
-
-        for (int i = 0 ; i < strLen ; i++) {
-            curChar = s.charAt(i);
-
-            number = charSet.getOrDefault(curChar, 0);
-
-            number = number + 1;
-
-            charSet.put(curChar, number);
+        int size = set.size();
+        if (size > 0) {
+            size -= 1;
         }
-    }
 
-    public static void main(String[] args) {
-        String str = "abccccdd";
-
-        LC627 lc627 = new LC627();
-
-        System.out.println(lc627.longestPalindrome(str));
+        return s.length() - size;
     }
 }
