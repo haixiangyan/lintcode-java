ffd134759a55e3e44eb45119192459f5423c9fde
Haixiang6123 <haixiang6123@gmail.com>
2018-09-27 13:55:19 -0700

    Review 415. 有效回文串

--- src/LC415.java
+++ src/LC415.java
@@ -1,52 +1,39 @@
 public class LC415 {
 
     public boolean isPalindrome(String s) {
-        // Check validation
-        if (s == null) {
-            return false;
-        }
-
-        if (s.equals("")) {
+        if (s == null || s.length() == 0) {
             return true;
         }
 
-        // Preprocess character
-        String newStr = preprocess(s);
+        int start = 0;
+        int end = s.length() - 1;
 
-        // Check validation
-        int strLen = newStr.length();
-        int mid = strLen / 2;
-        int left = mid - 1;
-        int right = strLen % 2 == 0 ? mid : mid + 1;
+        while (start < end) {
+            // Find the first valid character from left
+            while (start < s.length() && !isValid(s.charAt(start))) {
+                start++;
+            }
 
-        while (0 <= left && right < strLen) {
-            if (newStr.charAt(left) != newStr.charAt(right)) {
+            if (start == s.length()) {
+                return true;
+            }
+
+            while (end >= 0 && !isValid(s.charAt(end))) {
+                end--;
+            }
+
+            if (Character.toLowerCase(s.charAt(start)) != Character.toLowerCase(s.charAt(end))) {
                 return false;
             }
 
-            left --;
-            right ++;
+            start++;
+            end--;
         }
 
         return true;
     }
 
-    private String preprocess(String s) {
-        // Ignore cases
-        String newStr = s.toLowerCase();
-
-        // Remove invalid character
-        newStr = newStr.replaceAll("[^\\d\\w]", "");
-
-        System.out.println(newStr);
-        return newStr;
-    }
-
-    public static void main(String[] args) {
-        String str = "aaaa";
-
-        LC415 lc415 = new LC415();
-
-        System.out.println(lc415.isPalindrome(str));
+    private boolean isValid(char c) {
+        return Character.isLetter(c) || Character.isDigit(c);
     }
 }
