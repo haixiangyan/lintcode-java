39ff71b406eb38c95402d9ac3d1352364512082d
Haixiang6123 <haixiang6123@gmail.com>
2018-09-28 16:59:38 -0700

    Review 140. Fast Power

--- src/LC140.java
+++ src/LC140.java
@@ -3,29 +3,20 @@ Implement pow(x, n).
  */
 public class LC140 {
     public int fastPower(int a, int b, int n) {
+        if (n == 0) {
+            return 1 % b;
+        }
         if (n == 1) {
             return a % b;
         }
 
-        if (n == 0) {
-            return 1 % b;
+        long product = fastPower(a, b, n / 2);
+        product = product * product % b;
+
+        if (n % 2 == 1) {
+            product = (product  * a % b) % b;
         }
 
-        if (n % 2 == 0) {
-            long temp = fastPower(a, b,n / 2);
-            return (int) ((temp * temp) % b);
-        }
-        else {
-            long temp = fastPower(a, b, n / 2);
-            return (int) ((temp * temp) % b * a % b);
-        }
-    }
-
-    public static void main(String[] args) {
-        int a = 2147483647, b = 2147483645, n = 214748364;
-
-        LC140 lc140 = new LC140();
-
-        System.out.println(lc140.fastPower(a, b, n));
+        return (int)product;
     }
 }
