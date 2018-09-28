177d575e2d890c8a53ea3992513b9d0e2382e778
Haixiang6123 <haixiang6123@gmail.com>
2018-09-28 13:15:31 -0700

    Review 458. 目标最后位置

--- src/LC458.java
+++ src/LC458.java
@@ -5,37 +5,26 @@ public class LC458 {
         }
 
         int start = 0, end = nums.length - 1;
-
         while (start + 1 < end) {
-            int mid = (start + end) / 2;
+            int mid = start + (end - start) / 2;
             if (nums[mid] == target) {
                 start = mid;
             }
-            else if (nums[mid] < target) {
-                start = mid + 1;
+            else if (nums[mid] > target) {
+                end = mid;
             }
             else {
-                end = mid - 1;
+                start = mid;
             }
         }
 
         if (nums[end] == target) {
             return end;
         }
-
         if (nums[start] == target) {
             return start;
         }
 
         return -1;
-    }
-
-    public static void main(String[] args) {
-        LC458 lc458 = new LC458();
-
-        int[] nums = {1,2,4,5,5,6,6,6};
-        int target = 6;
-
-        System.out.println(lc458.lastPosition(nums, target));
     }
 }
