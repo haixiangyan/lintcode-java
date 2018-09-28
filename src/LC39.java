3901f1f243d7de91b92bd7f3fc038d87d4f23363
Haixiang6123 <haixiang6123@gmail.com>
2018-09-28 13:22:24 -0700

    Review 39. 恢复旋转排序数组

--- src/LC39.java
+++ src/LC39.java
@@ -2,62 +2,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LC39 {
-
     public void recoverRotatedSortedArray(List<Integer> nums) {
-        if (nums == null || nums.size() <= 1) {
+        if (nums == null || nums.size() == 0) {
             return;
         }
 
-        int len = nums.size();
-        int breakPoint;
-        // Find the break point
-        for (breakPoint = 0 ; breakPoint < len - 1 ; breakPoint++) {
-            if (nums.get(breakPoint) > nums.get(breakPoint + 1)) {
-                System.out.println("point " + breakPoint);
-                break;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums.get(i) > nums.get(i + 1)) {
+                reverse(nums, 0, i);
+                reverse(nums, i + 1, nums.size() - 1);
+                reverse(nums, 0, nums.size() - 1);
             }
         }
-
-        // Do left reverse
-        reverse(nums, 0, breakPoint);
-        // Do right reverse
-        reverse(nums, breakPoint + 1, len - 1);
-        // Do whole reverse
-        reverse(nums, 0, len - 1);
     }
 
-    private void reverse(List<Integer> nums, int start, int end) {
-        while (start <= end) {
-            int left = nums.get(start);
-            int right = nums.get(end);
-            int temp;
-
-            temp = left;
-            nums.set(start, right);
-            nums.set(end, temp);
-
-            start ++;
-            end --;
+    private void reverse(List<Integer> nums, int left, int right) {
+        for (int i = left, j = right; i < j; i++, j--) {
+            Integer temp = nums.get(i);
+            nums.set(i, nums.get(j));
+            nums.set(j, temp);
         }
-    }
-
-    private void print(List<Integer> nums) {
-        for (Integer i : nums) {
-            System.out.print(i + "\t");
-        }
-        System.out.println("----");
-    }
-
-    public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>();
-        list.add(4);
-        list.add(5);
-        list.add(1);
-        list.add(2);
-        list.add(3);
-
-        LC39 lc39 = new LC39();
-
-        lc39.recoverRotatedSortedArray(list);
     }
 }
