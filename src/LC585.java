656ec28d9f47d25fe90a0212de4aaf3ec6fb40e2
Haixiang6123 <haixiang6123@gmail.com>
2018-10-01 08:29:27 -0700

    Review 585. 山脉序列中的最大值

--- src/LC585.java
+++ src/LC585.java
@@ -5,32 +5,20 @@ Given nums = [10, 9, 8, 7], return 10
 public class LC585 {
     public int mountainSequence(int[] nums) {
         if (nums == null || nums.length == 0) {
-            return 0;
+            return -1;
         }
 
-        if (nums.length == 1) {
-            return nums[0];
-        }
-
-        int left = 0;
-        int right = nums.length - 1;
-        while (left < right) {
-            if (nums[left] >= nums[right]) {
-                right --;
+        int start = 0, end = nums.length - 1;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] < nums[mid + 1]) {
+                start = mid;
             }
-            else if (nums[left] < nums[right]) {
-                left ++;
+            else {
+                end = mid;
             }
         }
 
-        return nums[left] > nums[right] ? nums[left] : nums[right];
-    }
-
-    public static void main(String[] args) {
-        int[] nums = {3, 5, 3};
-
-        LC585 lc585 = new LC585();
-
-        System.out.println(lc585.mountainSequence(nums));
+        return Math.max(nums[start], nums[end]);
     }
 }
