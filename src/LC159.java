a485d136a0d3f13ee1541b3fad21d421db69ce06
Haixiang6123 <haixiang6123@gmail.com>
2018-10-01 10:00:41 -0700

    Review 159. 寻找旋转排序数组中的最小值

--- src/LC159.java
+++ src/LC159.java
@@ -1,36 +1,28 @@
 public class LC159 {
     public int findMin(int[] nums) {
         if (nums == null || nums.length == 0) {
-            return 0;
+            return -1;
         }
 
-        if (nums.length == 1) {
-            return nums[0];
-        }
-
-        int start = 0;
-        int end = nums.length - 1;
-        int target = nums[end];
-
+        int target = nums[nums.length - 1];
+        int start = 0, end = nums.length - 1;
         while (start + 1 < end) {
             int mid = start + (end - start) / 2;
-
-            if (nums[mid] >= target) {
-                start = mid;
+            if (nums[mid] < target) {
+                end = mid;
             }
             else {
-                end = mid;
+                start = mid;
             }
         }
 
-        return nums[start] >= nums[end] ? nums[end] : nums[start];
-    }
+        if (nums[start] < target) {
+            return start;
+        }
+        if (nums[end] < target) {
+            return end;
+        }
 
-    public static void main(String[] args) {
-        LC159 lc159 = new LC159();
-
-        int[] nums = {4, 5, 6, 7, 0, 1, 2};
-
-        System.out.println(lc159.findMin(nums));
+        return -1;
     }
 }
