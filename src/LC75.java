ad7a9749d75be1aa0fb1689773a42a53a970eac2
Haixiang6123 <haixiang6123@gmail.com>
2018-10-01 10:49:38 -0700

    Review 75. 寻找峰值

--- src/LC75.java
+++ src/LC75.java
@@ -1,28 +1,25 @@
 public class LC75 {
     public int findPeak(int[] A) {
-        int start = 0;
-        int end = A.length - 1;
-
-        while(start + 1 < end) {
+        int start = 0, end = A.length - 1;
+        while (start + 1 < end) {
             int mid = start + (end - start) / 2;
 
-            if (A[mid] > A[mid + 1]) {
-                end = mid;
-            } else if (A[mid - 1] < A[mid]) {
+            if (A[mid - 1] < A[mid]) {
                 start = mid;
-            } else {
+            }
+            else if (A[mid] > A[mid + 1]) {
+                end = mid;
+            }
+            else {
                 end = mid;
             }
         }
 
-        return A[start] > A[end] ? start : end;
-    }
-
-    public static void main(String[] args) {
-        int[] A = {1, 2, 1, 3, 4, 5, 7, 6};
-
-        LC75 lc75 = new LC75();
-
-        System.out.println(lc75.findPeak(A));
+        if (A[start] > A[end]) {
+            return start;
+        }
+        else {
+            return end;
+        }
     }
 }
