8ace8a47667ca8005a47043fceff3ae98e4643ff
Haixiang6123 <haixiang6123@gmail.com>
2018-10-03 15:29:47 -0700

    Review 600. 包裹黑色像素点的最小矩形

--- src/LC600.java
+++ src/LC600.java
@@ -1,32 +1,109 @@
 public class LC600 {
     public int minArea(char[][] image, int x, int y) {
-        if (image.length == 0) {
+        if (image == null || image.length == 0) {
+            return 0;
+        }
+        if (image[0] == null || image[0].length == 0) {
             return 0;
         }
 
-        int topIndex = findTop(image, x);
-        int bottomIndex = findBottom(image, x);
-        int leftIndex = findLeft(image, y);
-        int rightIndex = findRight(image, y);
+        int row = image.length;
+        int col = image[0].length;
 
-        if (topIndex == -1 || bottomIndex == -1 || leftIndex == -1 || rightIndex == -1) {
-            return 0;
-        }
+        int left = findLeft(image, 0, y);
+        int right = findRight(image, y, col - 1);
+        int up = findUp(image, 0, x);
+        int down = findDown(image, x, row - 1);
 
-        return (bottomIndex - topIndex + 1) * (rightIndex - leftIndex + 1);
+        return (right - left + 1) * (down - up + 1);
     }
 
-    private boolean isEmptyRow(char[][] image, int row) {
-        for (int i = 0 ; i < image[row].length ; i++) {
-            if (image[row][i] == '1') {
-                return false;
+    private int findLeft(char[][] image, int start, int end) {
+        int left = start, right = end;
+        while (left + 1 < right) {
+            int mid = left + (right - left) / 2;
+            if (isColEmpty(image, mid)) {
+                left = mid;
+            } else {
+                right = mid;
             }
         }
-        return true;
+
+        if (!isColEmpty(image, left)) {
+            return left;
+        }
+        if (!isColEmpty(image, right)) {
+            return right;
+        }
+        return -1;
     }
 
-    private boolean isEmptyCol(char[][] image, int col) {
-        for (int i = 0 ; i < image.length ; i++) {
+    private int findRight(char[][] image, int start, int end) {
+        int left = start, right = end;
+        while (left + 1 < right) {
+            int mid = left + (right - left) / 2;
+            if (isColEmpty(image, mid)) {
+                right = mid;
+            } else {
+                left = mid;
+            }
+        }
+
+        if (!isColEmpty(image, right)) {
+            return right;
+        }
+        if (!isColEmpty(image, left)) {
+            return left;
+        }
+
+        return -1;
+    }
+
+    private int findUp(char[][] image, int start, int end) {
+        int up = start, down = end;
+        while (up + 1 < down) {
+            int mid = up + (down - up) / 2;
+            if (isRowEmpty(image, mid)) {
+                up = mid;
+            } else {
+                down = mid;
+            }
+        }
+
+        if (!isRowEmpty(image, up)) {
+            return up;
+        }
+        if (!isRowEmpty(image, down)) {
+            return down;
+        }
+
+        return -1;
+    }
+
+    private int findDown(char[][] image, int start, int end) {
+        int up = start, down = end;
+        while (up + 1 < down) {
+            int mid = up + (down - up) / 2;
+            if (isRowEmpty(image, mid)) {
+                down = mid;
+            }
+            else {
+                up = mid;
+            }
+        }
+
+        if (!isRowEmpty(image, down)) {
+            return down;
+        }
+        if (!isRowEmpty(image, up)) {
+            return up;
+        }
+
+        return -1;
+    }
+
+    private boolean isColEmpty(char[][] image, int col) {
+        for (int i = 0; i < image.length; i++) {
             if (image[i][col] == '1') {
                 return false;
             }
@@ -35,118 +112,13 @@ public class LC600 {
         return true;
     }
 
-    private int findLeft(char[][] image, int y) {
-        int start = 0;
-        int end = y;
-
-        while (start + 1 < end) {
-            int mid = start + (end - start) / 2;
-
-            if (isEmptyCol(image, mid)) {
-                start = mid;
-            }
-            else {
-                end = mid;
+    private boolean isRowEmpty(char[][] image, int row) {
+        for (int i = 0; i < image[row].length; i++) {
+            if (image[row][i] == '1') {
+                return false;
             }
         }
 
-        if (!isEmptyCol(image, start)) {
-            return start;
-        }
-        if (!isEmptyCol(image, end)) {
-            return end;
-        }
-
-        return -1;
-    }
-
-    private int findRight(char[][] image, int y) {
-        int start = y;
-        int end = image[0].length - 1;
-
-        while (start + 1 < end) {
-            int mid = start + (end - start) / 2;
-
-            if (isEmptyCol(image, mid)) {
-                end = mid;
-            }
-            else {
-                start = mid;
-            }
-        }
-
-        if (!isEmptyCol(image, end)) {
-            return end;
-        }
-        if (!isEmptyCol(image, start)) {
-            return start;
-        }
-
-        return -1;
-    }
-
-    private int findTop(char[][] image, int x) {
-        int start = 0;
-        int end = x;
-
-        while (start + 1 < end) {
-            int mid = start + (end - start) / 2;
-
-            if (isEmptyRow(image, mid)) {
-                start = mid;
-            }
-            else {
-                end = mid;
-            }
-        }
-
-        if (!isEmptyRow(image, start)) {
-            return start;
-        }
-        if (!isEmptyRow(image, end)) {
-            return end;
-        }
-
-        return -1;
-    }
-
-    private int findBottom(char[][] image, int x) {
-        int start = x;
-        int end = image.length - 1;
-
-        while (start + 1 < end) {
-            int mid = start + (end - start) / 2;
-
-            if (isEmptyRow(image, mid)) {
-                end = mid;
-            }
-            else {
-                start = mid;
-            }
-        }
-
-        if (!isEmptyRow(image, end)) {
-            return end;
-        }
-        if (!isEmptyRow(image, start)) {
-            return start;
-        }
-
-        return -1;
-    }
-
-    public static void main(String[] args) {
-        char[][] image = {
-                {'0', '0', '1', '0'},
-                {'0', '1', '1', '0'},
-                {'0', '1', '0', '0'}
-        };
-
-        int x = 0;
-        int y = 2;
-
-        LC600 lc600 = new LC600();
-
-        System.out.println(lc600.minArea(image, x, y));
+        return true;
     }
 }
