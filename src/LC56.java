5186d5315fcefee63807febf3abe861444ebe2ea
Haixiang6123 <haixiang6123@gmail.com>
2018-10-04 09:50:21 -0700

    Review 56. 两数之和

--- src/LC56.java
+++ src/LC56.java
@@ -1,31 +1,47 @@
-import java.util.HashMap;
+import java.util.Arrays;
+import java.util.Comparator;
 
 public class LC56 {
-    public int[] twoSum(int[] numbers, int target) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-
-        for (int i = 0; i < numbers.length; i++) {
-            if (map.get(numbers[i]) != null) {
-                int[] result = { map.get(numbers[i]), i };
-                return result;
-            }
-            map.put(target - numbers[i], i);
+    class Pair {
+        int value;
+        int index;
+        public Pair(int value, int index) {
+            this.value = value;
+            this.index = index;
         }
-
-        int[] result = {};
-        return result;
     }
 
-    public static void main(String[] args) {
-        LC56 lc56 = new LC56();
-
-        int[] numbers = {1, 0, -1};
-        int target = -1;
-
-        int[] paris = lc56.twoSum(numbers, target);
-
-        for (Integer i : paris) {
-            System.out.println(i);
+    class PairComparator implements Comparator<Pair> {
+        @Override
+        public int compare(Pair p1, Pair p2) {
+            return p1.value - p2.value;
         }
+    }
+
+    public int[] twoSum(int[] numbers, int target) {
+        Pair[] pairs = new Pair[numbers.length];
+        for (int i = 0; i < numbers.length; i++) {
+            pairs[i] = new Pair(numbers[i], i);
+        }
+
+        Arrays.sort(pairs, new PairComparator());
+
+        int[] results = new int[2];
+        int left = 0, right = numbers.length - 1;
+        while (left < right) {
+            if (pairs[left].value + pairs[right].value == target) {
+                results[0] = Math.min(pairs[left].index, pairs[right].index);
+                results[1] = Math.max(pairs[right].index, pairs[left].index);
+                return results;
+            }
+            if (pairs[left].value + pairs[right].value < target) {
+                left++;
+            }
+            else {
+                right--;
+            }
+        }
+
+        return results;
     }
 }
