43b76a9ac175dc30bea6ef902f13481229029537
Haixiang6123 <haixiang6123@gmail.com>
2018-10-08 09:00:02 -0700

    Solve 102. 带环链表

--- src/LC102.java
+++ src/LC102.java
@@ -10,19 +10,22 @@ public class LC102 {
     }
 
     public boolean hasCycle(ListNode head) {
-        ListNode slow = head;
-        ListNode fast = head;
-
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-
-            // If slow can catch up fast then means it has a cycle
-            if (fast == slow) {
-                return true;
-            }
+        if (head == null || head.next == null) {
+            return false;
         }
 
-        return false;
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while (slow != fast) {
+            if (fast == null || fast.next == null) {
+                return false;
+            }
+
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return true;
     }
 }
