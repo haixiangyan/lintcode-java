498c39af9e8e49d74cbea4d3123f8ce1200ba4fb
Haixiang6123 <haixiang6123@gmail.com>
2018-10-08 16:01:36 -0700

    Review 5. 第k大元素

--- src/LC5.java
+++ src/LC5.java
@@ -1,52 +1,41 @@
 public class LC5 {
     public int kthLargestElement(int k, int[] nums) {
-        if (k > nums.length || k == 0 || nums.length == 0) {
+        if (nums == null) {
             return -1;
         }
 
-        return quickSelect(nums, 0, nums.length - 1, k);
+        return quickselect(nums, 0, nums.length - 1, k);
     }
 
-    private int quickSelect(int[] nums, int start, int end, int k) {
-        int left = start;
-        int right = end;
-        int pivot = nums[(start + end) / 2];
+    private int quickselect(int[] nums, int start, int end, int k) {
+        if (start >= end) {
+            return nums[start];
+        }
 
+        int left = start, right = end;
+        int pivot = nums[(start + end) / 2];
         while (left <= right) {
             while (left <= right && nums[left] > pivot) {
-                left ++;
+                left++;
             }
-            while (left <= right && nums[right] < pivot) {
-                right --;
+            while (left <= right && pivot > nums[right]) {
+                right--;
             }
-
             if (left <= right) {
                 int temp = nums[left];
                 nums[left] = nums[right];
                 nums[right] = temp;
-                left ++;
-                right --;
+                left++;
+                right--;
             }
         }
 
         if (start + k - 1 <= right) {
-            return quickSelect(nums, start, right, k);
+            return quickselect(nums, start, right, k);
         }
         if (start + k - 1 >= left) {
-            // Ignore elements on left side
-            return quickSelect(nums, left, end, k - (left - start));
+            return quickselect(nums, left, end, k - (left - start));
         }
-
-        // Pivot index
         return nums[right + 1];
-    }
-
-    public static void main(String[] args) {
-        int k = 3;
-        int[] nums = {9, 3, 2, 4, 8};
-
-        LC5 lc5 = new LC5();
-
-        System.out.println(lc5.kthLargestElement(k, nums));
     }
 }
