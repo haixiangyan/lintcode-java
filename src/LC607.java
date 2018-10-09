501de6fe6d99d8d38c401b1a60b652eaf8a29f05
Haixiang6123 <haixiang6123@gmail.com>
2018-10-09 10:43:09 -0700

    Review 607. Two Sum III - Data structure design

--- src/LC607.java
+++ src/LC607.java
@@ -3,45 +3,20 @@ import java.util.Iterator;
 import java.util.Map;
 
 public class LC607 {
-    private HashMap<Integer, Integer> map = new HashMap<>();
-    private int count = 0;
-
+    private Map<Integer, Integer> map = new HashMap<>();
     public void add(int number) {
-        count ++;
-        map.put(count, number);
+        map.put(number, map.getOrDefault(number, 0) + 1);
     }
 
     public boolean find(int value) {
-        Iterator<Map.Entry<Integer, Integer>> it = map.entrySet().iterator();
-
-        int first = 0;
-        boolean isCheck = false;
-
-        while (it.hasNext()) {
-            Map.Entry<Integer, Integer> entry = it.next();
-            if (map.containsValue(value - entry.getValue())) {
-                if (!isCheck) {
-                    first = entry.getKey();
-                    isCheck = true;
-                }
-                else {
-                    if (first != entry.getKey()) {
-                        return true;
-                    }
+        for (Integer key : map.keySet()) {
+            int diff = value - key;
+            if (map.containsKey(diff)) {
+                if (diff != key || map.get(key) > 1) {
+                    return true;
                 }
             }
         }
-
         return false;
-    }
-
-    public static void main(String[] args) {
-        LC607 lc607 = new LC607();
-
-        lc607.add(2);
-        lc607.add(3);
-
-        System.out.println(lc607.find(4));
-        System.out.println(lc607.find(7));
     }
 }
