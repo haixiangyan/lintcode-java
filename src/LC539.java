06e01171ca4e05340881f406c8a4091d4252c559
Haixiang6123 <haixiang6123@gmail.com>
2018-10-09 10:24:57 -0700

    Review 608. Two Sum II - Input array is sorted

--- src/LC539.java
+++ src/LC539.java
@@ -1,29 +1,14 @@
 public class LC539 {
     public void moveZeroes(int[] nums) {
-        int pos = 0;
-
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != 0) {
-                nums[pos] = nums[i];
-                pos++;
+        int left = 0, right = 0;
+        while (right < nums.length) {
+            if (nums[right] != 0) {
+                int temp = nums[right];
+                nums[right] = nums[left];
+                nums[left] = temp;
+                left++;
             }
-        }
-
-        // Set to zero
-        for (int i = pos; i < nums.length; i++) {
-            nums[i] = 0;
-        }
-    }
-
-    public static void main(String[] args) {
-        int[] nums = {0};
-
-        LC539 lc539 = new LC539();
-
-        lc539.moveZeroes(nums);
-
-        for (int i : nums) {
-            System.out.println(i);
+            right++;
         }
     }
 }
--- src/LC608.java
+++ src/LC608.java
@@ -1,35 +1,26 @@
 public class LC608 {
     public int[] twoSum(int[] nums, int target) {
-        int left = 0;
-        int right = nums.length - 1;
+        int[] results = new int[2];
+        if (nums == null || nums.length == 0) {
+            return results;
+        }
 
-        int[] pairs = new int[2];
-
+        int left = 0, right = nums.length - 1;
         while (left < right) {
-            if (nums[left] + nums[right] > target) {
+            int sum = nums[left] + nums[right];
+            if (sum > target) {
                 right--;
-            } else if (nums[left] + nums[right] < target) {
+            }
+            else if (sum < target) {
                 left++;
-            } else {
-                pairs[0] = Math.min(left + 1, right + 1);
-                pairs[1] = Math.max(left + 1, right + 1);
-                return pairs;
+            }
+            else {
+                results[0] = left + 1;
+                results[1] = right + 1;
+                return results;
             }
         }
 
-        return new int[0];
-    }
-
-    public static void main(String[] args) {
-        int[] nums = {2, 7, 11, 15};
-        int target = 9;
-
-        LC608 lc608 = new LC608();
-
-        int[] pairs = lc608.twoSum(nums, target);
-
-        for (int i : pairs) {
-            System.out.println(i);
-        }
+        return results;
     }
 }
