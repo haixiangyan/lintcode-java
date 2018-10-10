7a6863a9a5d112a7399cf52ce398eb976fe895e5
Haixiang <haixiang6123@gmail.com>
2018-10-10 08:59:56 -0700

    Review 533. 两数和的最接近值

--- src/LC533.java
+++ src/LC533.java
@@ -2,39 +2,25 @@ import java.util.Arrays;
 
 public class LC533 {
     public int twoSumClosest(int[] nums, int target) {
-        if (nums == null || nums.length <= 1) {
-            return 0;
+        if (nums == null || nums.length == 0) {
+            return Integer.MAX_VALUE;
         }
 
         Arrays.sort(nums);
 
-        int left = 0;
-        int right = nums.length - 1;
-        int min = Integer.MAX_VALUE;
-
+        int diff = Integer.MAX_VALUE;
+        int left = 0, right = nums.length - 1;
         while (left < right) {
-            if (Math.abs(target - (nums[left] + nums[right])) < min) {
-                min = Math.abs(target - (nums[left] + nums[right]));
-            }
-
-            if (nums[left] + nums[right] > target) {
-                right--;
-            } else if (nums[left] + nums[right] < target) {
+            if (nums[left] + nums[right] < target) {
+                diff = Math.min(diff, target - (nums[left] + nums[right]));
                 left++;
-            } else {
-                return 0;
+            }
+            else {
+                diff = Math.min(diff, (nums[left] + nums[right]) - target);
+                right--;
             }
         }
 
-        return min;
-    }
-
-    public static void main(String[] args) {
-        LC533 lc533 = new LC533();
-
-        int[] nums = {-1, 2, 1, -4};
-        int target = 4;
-
-        System.out.println(lc533.twoSumClosest(nums, target));
+        return diff;
     }
 }
