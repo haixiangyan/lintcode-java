7164224851a3264c578a93f2f9072edd423a506d
Haixiang <haixiang6123@gmail.com>
2018-10-10 08:47:31 -0700

    Review 587. 两数之和 - 不同组成

--- src/LC587.java
+++ src/LC587.java
@@ -2,44 +2,36 @@ import java.util.Arrays;
 
 public class LC587 {
     public int twoSum6(int[] nums, int target) {
-        if (nums == null || nums.length <= 1) {
+        if (nums == null || nums.length == 0) {
             return 0;
         }
 
         Arrays.sort(nums);
 
         int count = 0;
-        int left = 0;
-        int right = nums.length - 1;
-
+        int left = 0, right = nums.length - 1;
         while (left < right) {
-            if (nums[left] + nums[right] > target) {
-                right--;
-            } else if (nums[left] + nums[right] < target) {
-                left++;
-            } else {
+            int v = nums[left] + nums[right];
+            if (v == target) {
                 count++;
-                right--;
                 left++;
+                right--;
 
-                while (left < right && nums[left - 1] == nums[left]) {
+                // Skip duplicate
+                while (left < right && nums[left] == nums[left - 1]) {
                     left++;
                 }
                 while (left < right && nums[right] == nums[right + 1]) {
                     right--;
                 }
             }
+            else if (v > target) {
+                right--;
+            }
+            else {
+                left++;
+            }
         }
-
         return count;
-    }
-
-    public static void main(String[] args) {
-        LC587 lc587 = new LC587();
-
-        int[] nums = {7, 11, 11, 1, 2, 3, 4};
-        int target = 22;
-
-        System.out.println(lc587.twoSum6(nums, target));
     }
 }
