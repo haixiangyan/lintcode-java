e4d6bda663e5899bf685e3dc6f5c37ebdfbec8bd
Haixiang <haixiang6123@gmail.com>
2018-10-10 08:36:17 -0700

    Review 609. 两数和-小于或等于目标值

--- src/LC609.java
+++ src/LC609.java
@@ -6,30 +6,19 @@ public class LC609 {
             return 0;
         }
 
-        int left = 0;
-        int right = nums.length - 1;
-        int count = 0;
-
         Arrays.sort(nums);
 
+        int count = 0;
+        int left = 0, right = nums.length - 1;
         while (left < right) {
             if (nums[left] + nums[right] > target) {
                 right--;
-            } else {
+            }
+            else {
                 count = count + (right - left);
                 left++;
             }
         }
-
         return count;
-    }
-
-    public static void main(String[] args) {
-        LC609 lc609 = new LC609();
-
-        int[] nums = {2, 7, 11, 15};
-        int target = 24;
-
-        System.out.println(lc609.twoSum5(nums, target));
     }
 }
