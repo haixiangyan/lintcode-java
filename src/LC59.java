42c45d44c525d2404fb7d7400f6c84434a321109
Haixiang <haixiang6123@gmail.com>
2018-10-10 11:26:27 -0700

    Review 59. 3Sum Closest

--- src/LC59.java
+++ src/LC59.java
@@ -2,45 +2,34 @@ import java.util.Arrays;
 
 public class LC59 {
     public int threeSumClosest(int[] numbers, int target) {
-        if (numbers == null || numbers.length <= 2) {
+        if (numbers == null || numbers.length < 3) {
             return 0;
         }
 
         Arrays.sort(numbers);
 
-        int min = Integer.MAX_VALUE;
-        int minSum = 0;
+        int bestSum = numbers[0] + numbers[1] + numbers[2];
+        for (int i = 0; i < numbers.length - 2; i++) {
+            bestSum = twoSumClosest(numbers, i + 1, numbers.length - 1, numbers[i], target, bestSum);
+        }
 
-        for (int i = 0; i < numbers.length; i++) {
-            int left = i + 1;
-            int right = numbers.length - 1;
-            int newTarget = target - numbers[i];
+        return bestSum;
+    }
 
-            while (left < right) {
-                if (Math.abs(target - (numbers[left] + numbers[right] + numbers[i])) < min) {
-                    min = Math.abs(target - (numbers[left] + numbers[right] + numbers[i]));
-                    minSum = numbers[left] + numbers[right] + numbers[i];
-                }
-
-                if (numbers[left] + numbers[right] > newTarget) {
-                    right--;
-                } else if (numbers[left] + numbers[right] < newTarget) {
-                    left++;
-                } else {
-                    return numbers[left] + numbers[right] + numbers[i];
-                }
+    private int twoSumClosest(int[] numbers, int start, int end, int curNum, int target, int bestSum) {
+        while (start < end) {
+            int sum = curNum + numbers[start] + numbers[end];
+            if (Math.abs(target - sum) < Math.abs(target - bestSum)) {
+                bestSum = sum;
+            }
+            if (sum < target) {
+                start++;
+            }
+            else {
+                end--;
             }
         }
 
-        return minSum;
-    }
-
-    public static void main(String[] args) {
-        int[] numbers = {1,2,5,6,7,3,5,8,-33,-5,-72,12,-34,100,99,1,2,5,6,7,3,5,8,-33,-5,-72,12,-34,100,0,0,-2,2,-5,99,1,2,5,6,7,3,5,8,-33,-5,-72,12,-34,100,99,1,2,5,6,7,3,5,8,-33,-5,-72,12,-34,100,99,1,2,5,6,7,3,5,8,-33,-5,-72,12,-34,100,99,1,2,5,6,7,3,5,8,-33,-5,-72,12,-34,100,99,1,2,5,6,7,3,5,8,-33,-5,-72,12,-34,100,99,1,2,5,6,7,3,5,8,-33,-5,-72,12,-34,100,99,1,2,5,6,7,3,5,8,-33,-5,-72,12,-34,100,99,1,2,5,6,7,3,5,8,-33,-5,-72,12,-34,100,99,1,2,5,6,7,3,5,8,-33,-5,-72,12,-34,100,99,1,2,5,6,7,3,5,8,-33,-5,-72,12,-34,100,99,1,2,5,6,7,3,5,8,-33,-5,-72,12,-34,100,99,1,2,5,6,7,3,5,8,-33,-5,-72,12,-34,100,99,1,2,5,6,7,3,5,8,-33,-5,-72,12,-34,100,99,1,2,5,6,7,3,5,8,-33,-5,-72,12,-34,100,99,1,2,5,6,7,3,5,8,-33,-5,-72,12,-34,100,99,1,2,5,6,7,3,5,8,-33,-5,-72,12,-34,100,99,1,2,5,6,7,3,5,8,-33,-5,-72,12,-34,100,99,1,2,5,6,7,3,5,8,-33,-5,-72,12,-34,100,99};
-        int target = 25;
-
-        LC59 lc59 = new LC59();
-
-        System.out.println(lc59.threeSumClosest(numbers, target));
+        return bestSum;
     }
 }
