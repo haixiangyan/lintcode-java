c9423b13fd4a7c65b6438c5bb0b3cf4532951c0c
Haixiang <haixiang6123@gmail.com>
2018-10-10 11:10:12 -0700

    Review 382. Triangle Count

--- src/LC382.java
+++ src/LC382.java
@@ -2,43 +2,32 @@ import java.util.Arrays;
 
 public class LC382 {
     public int triangleCount(int[] S) {
-        if (S == null || S.length <= 2) {
+        if (S == null || S.length < 3) {
             return 0;
         }
 
         Arrays.sort(S);
 
         int count = 0;
-
         for (int i = 0; i < S.length; i++) {
-            count = count + twoSum(S, 0, i - 1, S[i]);
+            count = count + twoSumLarge(S, 0, i - 1, S[i]);
         }
 
         return count;
     }
 
-    private int twoSum(int[] S, int start, int end, int target) {
-        int left = start;
-        int right = end;
+    private int twoSumLarge(int[] S, int start, int end, int target) {
+        int left = start, right = end;
         int count = 0;
-
         while (left < right) {
             if (S[left] + S[right] > target) {
                 count = count + (right - left);
                 right--;
-            } else {
+            }
+            else {
                 left++;
             }
         }
-
         return count;
-    }
-
-    public static void main(String[] args) {
-        int[] S = {3,4,6,7};
-
-        LC382 lc382 = new LC382();
-
-        System.out.println(lc382.triangleCount(S));
     }
 }
