8b242a0b32cbdaa5070682cba5f4a7a806223d45
Haixiang <haixiang6123@gmail.com>
2018-10-16 10:14:50 -0700

    Review: 69. Binary Tree Level Order Traversal

--- src/LC69.java
+++ src/LC69.java
@@ -15,32 +15,31 @@ public class LC69 {
     }
 
     public List<List<Integer>> levelOrder(TreeNode root) {
-        Queue<TreeNode> queue = new LinkedList<>();
         List<List<Integer>> results = new ArrayList<>();
 
         if (root == null) {
             return results;
         }
 
-        queue.offer(root);
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
 
         while (!queue.isEmpty()) {
+            List<Integer> level = new ArrayList<>();
             int size = queue.size();
-            List<Integer> levelNodes = new ArrayList<>();
 
             for (int i = 0; i < size; i++) {
                 TreeNode node = queue.poll();
-                levelNodes.add(node.val);
-
+                level.add(node.val);
                 if (node.left != null) {
-                    queue.offer(node.left);
+                    queue.add(node.left);
                 }
                 if (node.right != null) {
-                    queue.offer(node.right);
+                    queue.add(node.right);
                 }
             }
 
-            results.add(levelNodes);
+            results.add(level);
         }
 
         return results;
