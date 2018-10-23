4144a8fbc1400e6c10018a3c26f8669e26fa0566
Haixiang <haixiang6123@gmail.com>
2018-10-23 12:35:34 -0700

    Review: 598. Zombie in Matrix

--- src/LC598.java
+++ src/LC598.java
@@ -2,62 +2,73 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class LC598 {
-    private final int WALL = 2;
-    private final int ZOMBIE = 1;
     private final int PEOPLE = 0;
+    private final int ZOMBIE = 1;
+    private final int WALL = 2;
 
-    class Coordinate {
-        int x;
-        int y;
-        Coordinate(int x, int y) {
+    private int[] dx = {1, 0, -1, 0};
+    private int[] dy = {0, 1, 0, -1};
+
+    class Point {
+        public int x;
+        public int y;
+
+        public Point(int x, int y) {
             this.x = x;
             this.y = y;
         }
     }
 
     public int zombie(int[][] grid) {
-        if (grid == null || grid.length == 0 || grid[0].length == 0) {
+        if (grid == null || grid.length == 0) {
+            return -1;
+        }
+        if (grid[0] == null || grid[0].length == 0) {
             return -1;
         }
 
-        int rowLen = grid.length;
-        int colLen = grid[0].length;
-        int[] detalX = {1, 0, -1, 0};
-        int[] detalY = {0, -1, 0, 1};
         int people = 0;
+        Queue<Point> queue = new LinkedList<>();
 
-        Queue<Coordinate> queue = new LinkedList<>();
-
-        for (int i = 0; i < rowLen; i++) {
-            for (int j = 0; j < colLen; j++) {
+        // Init queue
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
                 if (grid[i][j] == PEOPLE) {
                     people++;
-                }
-                else if (grid[i][j] == ZOMBIE) {
-                    queue.offer(new Coordinate(i, j));
+                } else if (grid[i][j] == ZOMBIE) {
+                    queue.add(new Point(i, j));
                 }
             }
         }
 
-        int days = 0;
+        if (people == 0) {
+            return 0;
+        }
 
+        // BFS
+        int days = 0;
         while (!queue.isEmpty()) {
             days++;
             int size = queue.size();
-
             for (int i = 0; i < size; i++) {
-                Coordinate node = queue.poll();
+                Point curPoint = queue.poll();
+                // Find next point
+                for (int direction = 0; direction < 4; direction++) {
+                    Point nextPoint = new Point(
+                            curPoint.x + dx[direction],
+                            curPoint.y + dy[direction]
+                    );
 
-                for (int j = 0; j < 4; j ++) {
-                    Coordinate neighbor = new Coordinate(node.x + detalX[j], node.y + detalY[j]);
-                    if (isInBound(neighbor, rowLen, colLen) && grid[neighbor.x][neighbor.y] == PEOPLE) {
-                        grid[neighbor.x][neighbor.y] = ZOMBIE;
-                        people--;
-                        if (people == 0) {
-                            return days;
-                        }
+                    if (!isInBound(grid, nextPoint)) {
+                        continue;
+                    }
 
-                        queue.offer(neighbor);
+                    queue.add(nextPoint);
+                    grid[nextPoint.x][nextPoint.y] = ZOMBIE;
+                    people--;
+
+                    if (people == 0) {
+                        return days;
                     }
                 }
             }
@@ -66,7 +77,14 @@ public class LC598 {
         return -1;
     }
 
-    private boolean isInBound(Coordinate coordinate, int rowLen, int colLen) {
-        return (coordinate.x >= 0) && (coordinate.x < rowLen) && (coordinate.y >= 0) && (coordinate.y < colLen);
+    private boolean isInBound(int[][] grid, Point nextPoint) {
+        if (nextPoint.x < 0 || nextPoint.x >= grid.length) {
+            return false;
+        }
+        if (nextPoint.y < 0 || nextPoint.y >= grid[0].length) {
+            return false;
+        }
+
+        return grid[nextPoint.x][nextPoint.y] == PEOPLE;
     }
 }
