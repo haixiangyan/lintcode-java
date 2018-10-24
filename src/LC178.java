2795cd80ef90fafcfb1bb10ed5c1ebf656b21c73
Haixiang <haixiang6123@gmail.com>
2018-10-24 14:21:34 -0700

    Review: 178. Graph Valid Tree

--- src/LC178.java
+++ src/LC178.java
@@ -2,46 +2,48 @@ import java.util.*;
 
 public class LC178 {
     public boolean validTree(int n, int[][] edges) {
-        if (n == 0) {
+        if (n <= 0) {
             return false;
         }
+        // Has cycles?
         if (edges.length != n - 1) {
             return false;
         }
 
-        Map<Integer, HashSet<Integer>> graph = createGraph(n, edges);
+        Map<Integer, Set<Integer>> graph = initializeGraph(n, edges);
 
         Queue<Integer> queue = new LinkedList<>();
         Set<Integer> set = new HashSet<>();
 
-        queue.offer(0);
+        queue.add(0);
         set.add(0);
 
         while (!queue.isEmpty()) {
-            int node = queue.poll();
-            for (Integer adjNode : graph.get(node)) {
-                if (!set.contains(adjNode)) {
-                    queue.offer(adjNode);
-                    set.add(adjNode);
+            Integer curNode = queue.poll();
+
+            for (Integer nextNode : graph.get(curNode)) {
+                if (!set.contains(nextNode)) {
+                    queue.add(nextNode);
+                    set.add(nextNode);
                 }
             }
         }
 
-        return (set.size() == n);
+        return set.size() == n;
     }
 
-    private Map<Integer, HashSet<Integer>> createGraph(int n, int[][] edges) {
-        Map<Integer, HashSet<Integer>> graph = new HashMap<>();
+    private Map<Integer, Set<Integer>> initializeGraph(int n, int[][] edges) {
+        Map<Integer, Set<Integer>> graph = new HashMap<>();
 
-        // Initialize graph
         for (int i = 0; i < n; i++) {
             graph.put(i, new HashSet<>());
         }
 
-        // Construct graph
-        for (int i = 0; i < edges.length; i++) {
-            graph.get(edges[i][0]).add(edges[i][1]);
-            graph.get(edges[i][1]).add(edges[i][0]);
+        for (int[] edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+            graph.get(u).add(v);
+            graph.get(v).add(u);
         }
 
         return graph;
