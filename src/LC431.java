b211241c928729e39a00d0c4a631b26e9eca70d9
Haixiang <haixiang6123@gmail.com>
2018-10-25 11:11:33 -0700

    Review: 431. Connected Component in Undirected Graph

--- src/LC431.java
+++ src/LC431.java
@@ -12,44 +12,43 @@ public class LC431 {
     }
 
     public List<List<Integer>> connectedSet(List<UndirectedGraphNode> nodes) {
-        List<List<Integer>> connectedGraphs = new ArrayList<>();
+        Map<UndirectedGraphNode, Boolean> visited = new HashMap<>();
+        List<List<Integer>> results = new ArrayList<>();
 
-        if (nodes == null || nodes.size() == 0) {
-            return connectedGraphs;
+        // Init visited
+        for (UndirectedGraphNode node : nodes) {
+            visited.put(node, false);
         }
 
-        Queue<UndirectedGraphNode> queue = new LinkedList<>();
-        Set<UndirectedGraphNode> set = new HashSet<>();
-
-        while (nodes.size() != 0) {
-            queue.offer(nodes.get(0));
-            set.add(nodes.get(0));
-
-            List<Integer> connectedGraph = new ArrayList<>();
-
-            while (!queue.isEmpty()) {
-                UndirectedGraphNode curNode = queue.poll();
-                connectedGraph.add(curNode.label);
-
-                for (UndirectedGraphNode adjNode : curNode.neighbors) {
-                    if (!set.contains(adjNode)) {
-                        queue.offer(adjNode);
-                        set.add(adjNode);
-                    }
-                }
-
-                nodes.remove(curNode);
+        // Iterate each node
+        for (UndirectedGraphNode node : nodes) {
+            if (!visited.get(node)) {
+                bfs(node, visited, results);
             }
-
-            connectedGraph.sort(new Comparator<Integer>() {
-                @Override
-                public int compare(Integer o1, Integer o2) {
-                    return o1 - o2;
-                }
-            });
-            connectedGraphs.add(connectedGraph);
         }
 
-        return connectedGraphs;
+        return results;
+    }
+
+    private void bfs(UndirectedGraphNode node, Map<UndirectedGraphNode, Boolean> visited, List<List<Integer>> results) {
+        List<Integer> connectedGraph = new ArrayList<>();
+        Queue<UndirectedGraphNode> queue = new LinkedList<>();
+        queue.add(node);
+
+        while (!queue.isEmpty()) {
+            UndirectedGraphNode curNode = queue.poll();
+            visited.put(curNode, true);
+            connectedGraph.add(curNode.label);
+
+            for (UndirectedGraphNode nextNode : curNode.neighbors) {
+                if (!visited.get(nextNode)) {
+                    queue.add(nextNode);
+                    visited.put(nextNode, true);
+                }
+            }
+        }
+
+        Collections.sort(connectedGraph);
+        results.add(connectedGraph);
     }
 }
