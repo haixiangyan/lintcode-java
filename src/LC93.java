a2701e4fece8ee6dafc404130fe7c480175143da
Haixiang <haixiang6123@gmail.com>
2018-11-01 13:19:44 -0700

    Review: 93. Balanced Binary Tree

--- src/LC93.java
+++ src/LC93.java
@@ -12,7 +12,6 @@ public class LC93 {
     class ResultType {
         public boolean isBalanced;
         public int depth;
-
         public ResultType(boolean isBalanced, int depth) {
             this.isBalanced = isBalanced;
             this.depth = depth;
@@ -20,24 +19,29 @@ public class LC93 {
     }
 
     public boolean isBalanced(TreeNode root) {
-        return check(root).isBalanced;
+        if (root == null) {
+            return true;
+        }
+
+        return helper(root).isBalanced;
     }
 
-    private ResultType check(TreeNode root) {
+    private ResultType helper(TreeNode root) {
         if (root == null) {
             return new ResultType(true, 0);
         }
 
-        ResultType leftResult = check(root.left);
-        ResultType rightResult = check(root.right);
+        ResultType left = helper(root.left);
+        ResultType right = helper(root.right);
 
-        if (!leftResult.isBalanced || !rightResult.isBalanced) {
-            return new ResultType(false, -1);
-        }
-        if (Math.abs(leftResult.depth - rightResult.depth) > 1) {
+        if (!left.isBalanced || !right.isBalanced) {
             return new ResultType(false, -1);
         }
 
-        return new ResultType(true, Math.max(leftResult.depth, rightResult.depth) + 1);
+        if (Math.abs(left.depth - right.depth) > 1) {
+            return new ResultType(false, -1);
+        }
+
+        return new ResultType(true, Math.max(left.depth, right.depth) + 1);
     }
 }
