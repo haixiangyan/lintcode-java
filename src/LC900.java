4013070d2ebdb899cecacc3492042273d4513f5b
Haixiang <haixiang6123@gmail.com>
2018-11-02 10:42:03 -0700

    Review: 900. Closest Binary Search Tree Value

--- src/LC900.java
+++ src/LC900.java
@@ -14,53 +14,58 @@ public class LC900 {
             return 0;
         }
 
-        TreeNode lowerNode = lowerBound(root, target);
-        TreeNode upperNode = upperBound(root, target);
+        // Find largest number that is smaller than target
+        TreeNode lowerbound = findLowerBound(root, target);
+        // Find smallest number that is larger than target
+        TreeNode upperbound = findUpperBound(root, target);
 
-        if (lowerNode == null) {
-            return upperNode.val;
+        if (lowerbound == null) {
+            return upperbound.val;
         }
-        if (upperNode == null) {
-            return lowerNode.val;
+        if (upperbound == null) {
+            return lowerbound.val;
         }
 
-        if (Math.abs(target - lowerNode.val) > Math.abs(target - upperNode.val)) {
-            return upperNode.val;
+        if (target - lowerbound.val > upperbound.val - target) {
+            return upperbound.val;
         }
-        else {
-            return lowerNode.val;
-        }
+
+        return lowerbound.val;
     }
 
-    private TreeNode lowerBound(TreeNode root, double target) {
+    private TreeNode findLowerBound(TreeNode root, double target) {
         if (root == null) {
             return null;
         }
 
-        if (target <= root.val) {
-            return lowerBound(root.left, target);
+        if (root.val > target) {
+            return findLowerBound(root.left, target);
         }
 
-        TreeNode lowerNode = lowerBound(root.right, target);
-        if (lowerNode != null) {
-            return lowerNode;
+        // root.val <= target
+        TreeNode lowerBound = findLowerBound(root.right, target);
+        if (lowerBound != null) {
+            return lowerBound;
         }
+
         return root;
     }
 
-    private TreeNode upperBound(TreeNode root, double target) {
+    private TreeNode findUpperBound(TreeNode root, double target) {
         if (root == null) {
             return null;
         }
 
-        if (root.val <= target) {
-            return upperBound(root.right, target);
+        if (root.val < target) {
+            return findUpperBound(root.right, target);
         }
 
-        TreeNode upperNode = upperBound(root.left, target);
-        if (upperNode != null) {
-            return upperNode;
+        // root.val >= target
+        TreeNode upperBound = findUpperBound(root.left, target);
+        if (upperBound != null) {
+            return upperBound;
         }
+
         return root;
     }
 }
