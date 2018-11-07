633e14494e51b28225a9cba1196b1de9ce840cf2
Haixiang <haixiang6123@gmail.com>
2018-11-07 11:14:24 -0800

    Review: 595. Binary Tree Longest Consecutive Sequence

--- src/LC595.java
+++ src/LC595.java
@@ -9,24 +9,52 @@ public class LC595 {
         }
     }
 
+    class ResultType {
+        public int maxInSubtree;
+        public int maxFromRoot;
+        public ResultType(int maxInSubtree, int maxFromRoot) {
+            this.maxInSubtree = maxInSubtree;
+            this.maxFromRoot = maxFromRoot;
+        }
+    }
+
     public int longestConsecutive(TreeNode root) {
         if (root == null) {
             return 0;
         }
 
-        return helper(root, null, 1);
+        return divideConquer(root).maxInSubtree;
     }
 
-    private int helper(TreeNode root, TreeNode parent, int lengthWithoutRoot) {
+    private ResultType divideConquer(TreeNode root) {
         if (root == null) {
-            return 0;
+            return new ResultType(0, 0);
         }
 
-        int length = (parent != null && parent.val + 1 == root.val) ? lengthWithoutRoot + 1 : 1;
+        ResultType left = divideConquer(root.left);
+        ResultType right = divideConquer(root.right);
 
-        int left = helper(root.left, root, length);
-        int right = helper(root.right, root, length);
+        // 加上 root，当前就是1
+        ResultType result = new ResultType(0, 1);
 
-        return Math.max(length, Math.max(left, right));
+        if (root.left != null && root.val + 1 == root.left.val) {
+            result.maxFromRoot = Math.max(
+                    result.maxFromRoot,
+                    left.maxFromRoot + 1
+            );
+        }
+        if (root.right != null && root.val + 1 == root.right.val) {
+            result.maxFromRoot = Math.max(
+                    result.maxFromRoot,
+                    right.maxFromRoot + 1
+            );
+        }
+
+        result.maxInSubtree = Math.max(
+                result.maxFromRoot,
+                Math.max(left.maxInSubtree, right.maxInSubtree)
+        );
+
+        return result;
     }
 }
